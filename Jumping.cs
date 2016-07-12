60ce79347196bca0979715f93cc6b73b76368d45
Juanjo20 <juanjito2040@gmail.com>
2016-07-12 00:20:21 -0500

    Add files via upload

---
+++ Jumping.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Jumping : MonoBehaviour {
+
+	// Use this for initialization
+
+
+	public GameObject Ball;
+	public float position_X;
+	public float position_Y;
+	public int Vel_Total;
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Vel_Total > 0) {
+			return;
+		} else {
+			position_X += transform.position.x (Ball,position_X) as GameObject;
+			position_Y += transform.position.y (Ball, position_Y) as GameObject;
+
+		}
+	}
+}
